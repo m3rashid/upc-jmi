505bfc5f866608a6394c9879911e1c9dd96fbb05
m3rashid <mdrashid.hussain.786.01@gmail.com>
2022-05-17 22:51:09 +0530

    updates

--- client/src/pages/academic/facultyMembers/index.tsx
+++ client/src/pages/academic/facultyMembers/index.tsx
@@ -1,14 +1,51 @@
 import React from 'react'
+import { Avatar, Card, Group, SimpleGrid, Text, Title } from '@mantine/core'
 
-import { useStyles } from './styles'
+import { useGlobalStyles } from '../../../globals/globalStyles'
+import TwoColumnGrid from '../../../globals/twoColumnGrid'
+import { facilityList } from './data'
 
 interface IProps {}
 
 const FacultyMembers: React.FC<IProps> = () => {
-  const { classes } = useStyles()
+  const { classes: globalClasses } = useGlobalStyles()
+
   return (
     <>
-      <div>FacultyMembers</div>
+      <Title className={globalClasses.themeColor} mb={20} ml={5}>
+        Faculty Members
+      </Title>
+      <TwoColumnGrid>
+        <SimpleGrid>
+          {facilityList.map((faculty) => (
+            <Card key={faculty.id}>
+              <Group noWrap>
+                <Avatar src={faculty.avatar} size={75} radius="md" />
+                <div>
+                  <Text
+                    size="xs"
+                    sx={{ textTransform: 'uppercase' }}
+                    weight={700}
+                    color="dimmed"
+                  >
+                    {faculty.department}
+                  </Text>
+
+                  <Text size="lg" weight={500}>
+                    {faculty.name}
+                  </Text>
+                </div>
+              </Group>
+              <Group noWrap spacing={10} mt={10}>
+                <Text size="sm" color="dimmed">
+                  {faculty.bio}
+                </Text>
+              </Group>
+            </Card>
+          ))}
+        </SimpleGrid>
+        <div></div>
+      </TwoColumnGrid>
     </>
   )
 }
---
+++ client/src/pages/academic/phdScholars/data.ts
@@ -0,0 +1,7 @@
+export interface IPhdScholar {
+  id: string
+  name: string
+  title: string
+  supervisor: string
+  registrationYear: number | string
+}
