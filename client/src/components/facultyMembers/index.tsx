c8c6ef072b4df3c17085626f13d4a598c8f224d8
Faizan Choudhary <faizan2000461@st.jmi.ac.in>
2022-05-18 16:18:30 +0530

    added faculty and staff details

--- client/src/components/facultyMembers/index.tsx
+++ client/src/components/facultyMembers/index.tsx
@@ -1,21 +1,5 @@
 import React from 'react'
-
-export interface IString {
-  id: string
-  text: string
-}
-
-export interface IDetail {
-  id: string
-  name: string
-  department: string
-  address: string
-  phone: string
-  emails: string[]
-  description: IString[]
-  classesTaught: IString[]
-  coursesTaught: IString[]
-}
+import { facultyDetails } from './data'
 
 interface IProps {}
 
